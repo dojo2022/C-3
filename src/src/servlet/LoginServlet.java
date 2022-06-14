430b58758fd573908f59a026bd82973caafd7848
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-14 14:57:33 +0900

    Update LoginServlet.java

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -30,8 +30,9 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+
+		response.sendRedirect("/app/HomeServlet");
 	}
 
 }
