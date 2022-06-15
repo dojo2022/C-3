af2bd71dd9415044ff611ac69fdbb34e85e3b6fc
dojo2022kawakami <103239295+dojo2022kawakami@users.noreply.github.com>
2022-06-15 17:03:15 +0900

    Update registar.js

--- src/WebContent/js/registar.js
+++ src/WebContent/js/registar.js
@@ -14,6 +14,23 @@ function pushHideButton() {
 
     	}
     };
+
+   //パスワード確認用フォーム
+//function CheckPassword(password_confirm){
+
+    //入力値取得
+
+   // var input1 = password.value;
+   // var input2 = password_confirm.value;
+
+    //パスワード比較
+   // if(input1 != input2){
+       // password_confirm.setCustomValidity("存在しています")
+   // }else{
+        //password_confirm.setCustomValidity("")
+   // }
+   // }
+
  /**
  *
  */
