52d7a2b2625bdc4824aa9896fd7502d76d870dc7
dojo2022-watanabe <103237421+dojo2022-watanabe@users.noreply.github.com>
2022-06-15 14:33:23 +0900

    ランキングサーブレット

--- src/src/servlet/RankingServlet.java
+++ src/src/servlet/RankingServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.loginDao;
+import model.user;
+
 /**
  * Servlet implementation class RankingServlet
  */
@@ -29,17 +32,18 @@ public class RankingServlet extends HttpServlet {
 			return;
 		}
 
+		//検索処理を行う
+		//領分的にはloginDaoだけど、joysDaoで扱った方がいいのか、新規Daoを作成すべきか？
+		loginDao loginDao = new loginDao();
+		List<user> ranking = loginDao.select(new user());
 
-		//	ランキングにフォワードする
+		//検索結果をリクエストスコープに格納する
+		request.setAttribute("ranking", loginDao);
+
+		//	ランキング＝結果ページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/ranking.jsp");
 		dispatcher.forward(request, response);
 	}
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+
 
 }
