5db731864747098b6d28a421da06942d0ea3fbff
dojo2022kawakami <103239295+dojo2022kawakami@users.noreply.github.com>
2022-06-15 14:38:22 +0900

    Update LoginServlet.java

--- src/src/servlet/LoginServlet.java
+++ src/src/servlet/LoginServlet.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import dao.loginDao;
+import model.result;
 import model.user;
 
 /**
@@ -37,23 +39,42 @@ public class LoginServlet extends HttpServlet {
 			throws ServletException, IOException {
 
 		//dao isLoginOKをつかって、データベースと情報と比較してokなら
-		
-		
-		// セッションスコープにIDを格納する
+		//リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		String user_id = request.getParameter("user_id");
+		String password = request.getParameter("password");
+		String nickname = request.getParameter("nickname");
+		int having_point =Integer.parseInt(request.getParameter("having_point"));
+		int total_point = Integer.parseInt(request.getParameter("total_point"));
+		String picture = request.getParameter("picture");
+
+		//ログイン処理を行う
+		loginDao lDao = new loginDao();
+		if (lDao.isLoginOK(new user(user_id, password,nickname,having_point,total_point,picture))) {	// ログイン成功
+
+		// セッションスコープにIDを格納する(先生の書いたやつ）
+		//HttpSession session = request.getSession();
+		//user user = new user();
+		//user.setUser_id("makoto");
+		//session.setAttribute("id", user);
+
+		//セッションスコープにIDを格納
 		HttpSession session = request.getSession();
-		user user = new user();
-		user.setUser_id("makoto");
-		session.setAttribute("id", user);
-		
-		
-		
-
-
-
+		session.setAttribute("id", new user(user_id,password,nickname,having_point,total_point,picture));
 
 
 		// ホームサーブレットにリダイレクトする
 		response.sendRedirect("/app/HomeServlet");
 	}
+		else {									// ログイン失敗
+			// リクエストスコープに、タイトル、メッセージ、戻り先を格納する
+			request.setAttribute("result",
+			new result("ログイン失敗！", "IDまたはPWに間違いがあります。", "/app/LoginServlet","ログインページへ戻る"));
 
+			// 結果ページにフォワードする
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/result.jsp");
+			dispatcher.forward(request, response);
+		}
+
+}
 }
