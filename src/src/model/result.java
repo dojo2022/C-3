f0448fe57d129115dd0be06ef710779622bf7d74
dojo2022-watanabe <103237421+dojo2022-watanabe@users.noreply.github.com>
2022-06-15 09:53:53 +0900

    ResultのjavaBeans

--- src/src/model/result.java
+++ src/src/model/result.java
@@ -10,5 +10,33 @@ import java.io.Serializable;
  *
  */
 public class result implements Serializable {
-
+	private String title;
+	private String message;
+	private String backTo;
+	private String backToMessage;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getBackTo() {
+		return backTo;
+	}
+	public void setBackTo(String backTo) {
+		this.backTo = backTo;
+	}
+	public String getBackToMessage() {
+		return backToMessage;
+	}
+	public void setBackToMessage(String backToMessage) {
+		this.backToMessage = backToMessage;
+	}
 }
+
