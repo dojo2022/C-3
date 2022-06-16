4011dee42632be11d26c819fc80f8e398935a033
dojo2022kawakami <103239295+dojo2022kawakami@users.noreply.github.com>
2022-06-16 11:27:47 +0900

    Update loginDao.java

--- src/src/dao/loginDao.java
+++ src/src/dao/loginDao.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import model.user;
 
@@ -114,4 +116,78 @@ public class loginDao {
 		// 結果を返す
 		return newuser;
 	}
+
+
+//プロフィールの表示
+//引数paramで検索項目を指定し、検索結果のリストを返す
+	public List<user> select(user param) {
+		Connection conn = null;//初期化
+		List<user> profile = new ArrayList<user>();
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+			//ドライバ（DBMSを操作するプログラム）を読み込む作業、h2のドライバに接続するときの名前
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6_data/C3", "sa", "");
+			//データーベースに接続するためのユーザーIDとPW、データベースのh2コンソールから持ってくる
+
+			// SQL文を準備する<<ここに改造>>
+			String sql = "SELECT user_id, password, nickname, having_point, total_point, picture FROM user WHERE user_id=?";
+			PreparedStatement pStmt = conn.prepareStatement(sql);//connはConnectionクラスのインスタンス、SQL文に入っている？に後から入力できるようにした
+
+			// SQL文を完成させる　？の数だけセットする必要がある
+			if (param.getUser_id() != null) {
+				pStmt.setString(1, param.getUser_id() );
+			}//最初に出た？1個目という意味
+
+
+
+			// SQL文を実行し、結果表を取得する
+			ResultSet rs = pStmt.executeQuery();
+
+			// 結果表をコレクションにコピーする<<ここも改造>>検索した結果を格納
+			while (rs.next()) {
+				user usercard = new user(
+				rs.getString("user_id"),
+				rs.getString("password"),
+				rs.getString("nickname"),
+				rs.getInt("having_point"),
+				rs.getInt("total_point"),
+				rs.getString("picture")
+
+				);
+				profile.add(usercard);
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			profile = null;
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			profile = null;
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+					profile = null;
+				}
+			}
+		}
+
+		// 結果を返す
+		return profile;
+	}
 }
+
+
+
+
+
