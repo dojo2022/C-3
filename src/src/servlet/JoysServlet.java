ac92a4f44f2bc6394c842bb05e430360aa74d7e2
pengin44 <102942799+pengin44@users.noreply.github.com>
2022-06-16 11:14:42 +0900

    Update JoysServlet.java

--- src/src/servlet/JoysServlet.java
+++ src/src/servlet/JoysServlet.java
@@ -35,6 +35,34 @@ public class JoysServlet extends HttpServlet {
 		}
 		 */
 
+		// もしもログインしていなかったらログインサーブレットにリダイレクトする
+		HttpSession session = request.getSession();
+		user user = (user)session.getAttribute("id");
+
+		System.out.println(user.getUser_id());
+
+		joysDao dao = new joysDao();
+
+		//パターン１
+		//引数をidを文字列とした場合
+		dao.select(user.getUser_id());
+
+		//リクエストパラメータを取得する
+/* 		  名刺管理アプリではJSPにフォームの記載があったためリクパラ必須だった(フォームで送られたデータを取得するため)
+		request.setCharacterEncoding("UTF-8");
+		String reward_id = request.getParameter("reward_id");
+		String user_id = request.getParameter("user_id");
+		String reward_name = request.getParameter("reward_name");
+		String reward_detail = request.getParameter("reward_detail");
+*/
+
+		// 検索処理を行う
+		joysDao jDao = new joysDao();
+		List<reward> rewardList = jDao.select( user.getUser_id());
+
+		// 検索結果をリクエストスコープに格納する
+				request.setAttribute("rewardList", rewardList);
+
 		// joys一覧閲覧画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/joys.jsp");
 		dispatcher.forward(request, response);
@@ -45,31 +73,8 @@ public class JoysServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// もしもログインしていなかったらログインサーブレットにリダイレクトする
-		HttpSession session = request.getSession();
-		user user = (user)session.getAttribute("id");
 
-		System.out.println(user.getUser_id());
 
-		joysDao dao = new joysDao();
-		//パターン１
-		//引数をidを文字列とした場合
-		dao.select(user.getUser_id());
-
-		//リクエストパラメータを取得する
-
-		request.setCharacterEncoding("UTF-8");
-		String reward_id = request.getParameter("reward_id");
-		String user_id = request.getParameter("user_id");
-		String reward_name = request.getParameter("reward_name");
-		String reward_detail = request.getParameter("reward_detail");
-
-		// 検索処理を行う
-		joysDao jDao = new joysDao();
-		List<reward> rewardList = jDao.select(new reward(reward_id,user_id,reward_name,reward_detail));
-
-		// 検索結果をリクエストスコープに格納する
-				request.setAttribute("rewardList", rewardList);
 
 	}
 
