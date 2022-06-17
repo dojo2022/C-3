4af614464b62251f95628ac59984af1e30dd265e
dojo2022-ishiguro <103226179+dojo2022-ishiguro@users.noreply.github.com>
2022-06-17 12:03:33 +0900

    Update profile.js

--- src/WebContent/js/profile.js
+++ src/WebContent/js/profile.js
@@ -3,12 +3,12 @@
 var formObj = document.getElementById('profile_form');
 
 /*編集するが選択されているときの処理*/
-document.puerySelecter('input[value ="yes"]').onchang = function(){
+document.querySelector('input[value ="yes"]').onchange = function(){
 formObj.update.disabled = false;
 }
 
 
 /*編集しないが選択されているときの処理*/
- document.puerySelecter('input[value ="no"]').onchang = function(){
+ document.querySelector('input[value ="no"]').onchange = function(){
 formObj.update.disabled = true;
 }
