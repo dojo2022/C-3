2764d7fdf671b692dc3816485a1083cc6a8cc51b
pengin44 <102942799+pengin44@users.noreply.github.com>
2022-06-17 10:17:11 +0900

    Update JoysServlet.java

--- src/src/servlet/JoysServlet.java
+++ src/src/servlet/JoysServlet.java
@@ -21,46 +21,34 @@ import model.user;
 @WebServlet("/JoysServlet")
 public class JoysServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private HttpServletRequest request;
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-/*
+
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession sessiont = request.getSession();
-		if (sessiont.getAttribute("user_id") == null) {
+		if (sessiont.getAttribute("id") == null) {
 			response.sendRedirect("/app/LoginServlet");
 			System.out.println(sessiont);
 			return;
-*/
+
 		}//31 32 sessiont
 
-/*
+
 		// もしもログインしていなかったらログインサーブレットにリダイレクトする
 		HttpSession session = request.getSession();
 		user user = (user)session.getAttribute("id");
-*/
-/*
+
+
 		//セッションスコープにIDを格納
-		HttpSession session = request.getSession();
-		user user = (user)session.getAttribute("id");
-*/
+//	HttpSession session = request.getSession();
+//		user user = (user)session.getAttribute("id");
+
 		//System.out.println(get.user_id());
 
-		//パターン１
-		//引数をidを文字列とした場合
-		//dao.select(user.getUser_id());
-
-
-		//リクエストパラメータを取得する
-/* 		  名刺管理アプリではJSPにフォームの記載があったためリクパラ必須だった(フォームで送られたデータを取得するため)
-		request.setCharacterEncoding("UTF-8");
-		String reward_id = request.getParameter("reward_id");
-		String user_id = request.getParameter("user_id");
-		String reward_name = request.getParameter("reward_name");
-		String reward_detail = request.getParameter("reward_detail");
-*/
 
 		// 検索処理を行う
 		joysDao jDao = new joysDao();
@@ -72,7 +60,7 @@ public class JoysServlet extends HttpServlet {
 		// joys一覧閲覧画面にフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/joys.jsp");
 		dispatcher.forward(request, response);
-	}
+		}
 
 
 	/**
