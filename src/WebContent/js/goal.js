bfef392e1e3d1fb258ec611ac03a0ca2307205e6
dojo2022-jomaru <103240307+dojo2022-jomaru@users.noreply.github.com>
2022-06-17 17:03:08 +0900

    Update goal.js

--- src/WebContent/js/goal.js
+++ src/WebContent/js/goal.js
@@ -1,11 +1,32 @@
 /**
  *
  */
- 'use strict';
+    let btn0 = document.getElementById('btn0');
+    btn0.onclick = function(){
+        f0.submit();
+    }
 
- $(function() {
-  $('.title').click(function() {
-    $(this).next('.content').slideToggle();
-    $(this).toggleClass("active");
-  });
-});
+    let btn1 = document.getElementById('btn1');
+    btn1.onclick = function(){
+        f1.submit();
+    }
+
+    let btn2 = document.getElementById('btn2');
+    btn2.onclick = function(){
+        f2.submit();
+    }
+
+    let btn3 = document.getElementById('btn3');
+    btn3.onclick = function(){
+        f3.submit();
+	}
+
+	let btn4 = document.getElementById('btn4');
+    btn4.onclick = function(){
+        f4.submit();
+	}
+
+	let btn5 = document.getElementById('btn5');
+    btn5.onclick = function(){
+        f5.submit();
+        }
