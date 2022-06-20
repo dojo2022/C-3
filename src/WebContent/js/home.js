8715cf0fb2fd13e5022ce67f0ed39ec381a0643d
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-20 11:39:16 +0900

    Update home.js

--- src/WebContent/js/home.js
+++ src/WebContent/js/home.js
@@ -1,23 +1,34 @@
 /**
  *
  */
- 'use strict';
- function exec(){
- 	document.getElementById("tab0").submit();
- }
- function onRadioButtoChange(){
 
-	//ラジオボタンが押されているかどうか調べて変数に入れている
-	check0 = document.tag_id0.tag.checked;
-	check1 = document.tag_id0.tag.checked;
-	check2 = document.tag_id0.tag.checked;
-	check3 = document.tag_id0.tag.checked;
-	check4 = document.tag_id0.tag.checked;
-	check5 = document.tag_id0.tag.checked;
 
-	if(check0 = true){
-		document.getElementById("tag_id0").submit();
-	} else if(check1 = true){
-		document.getElementById("tag_id1").submit();
+ let btn0 = document.getElementById('btn0');
+    btn0.onclick = function(){
+        f0.submit();
+    }
+
+    let btn1 = document.getElementById('btn1');
+    btn1.onclick = function(){
+        f1.submit();
+    }
+
+    let btn2 = document.getElementById('btn2');
+    btn2.onclick = function(){
+        f2.submit();
+    }
+
+    let btn3 = document.getElementById('btn3');
+    btn3.onclick = function(){
+        f3.submit();
 	}
-}
+
+	let btn4 = document.getElementById('btn4');
+    btn4.onclick = function(){
+        f4.submit();
+	}
+
+	let btn5 = document.getElementById('btn5');
+    btn5.onclick = function(){
+        f5.submit();
+        }
