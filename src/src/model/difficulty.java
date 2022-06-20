36780729d20f0b4fa1b8744e95d643fcfbf16fa5
dojo2022-ishiguro <103226179+dojo2022-ishiguro@users.noreply.github.com>
2022-06-20 13:03:24 +0900

    Update difficulty.java

--- src/src/model/difficulty.java
+++ src/src/model/difficulty.java
@@ -5,15 +5,15 @@ import java.io.Serializable;
 public class difficulty implements Serializable {
 	private String difficulty_id;
 	private String difficulty_name;
-	private int goal_name;
+	private int goal_point;
 	public String getDifficulty_id() {
 		return difficulty_id;
 	}
 	public String getDifficulty_name() {
 		return difficulty_name;
 	}
-	public int getGoal_name() {
-		return goal_name;
+	public int getGoal_point() {
+		return goal_point;
 	}
 	public void setDifficulty_id(String difficulty_id) {
 		this.difficulty_id = difficulty_id;
@@ -22,7 +22,7 @@ public class difficulty implements Serializable {
 		this.difficulty_name = difficulty_name;
 	}
 	public void setGoal_name(int goal_name) {
-		this.goal_name = goal_name;
+		this.goal_point = goal_name;
 	}
 
 
