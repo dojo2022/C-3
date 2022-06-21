6f14ce85e4b924931c043d2fff07c912183157c1
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-21 15:05:31 +0900

    Update CheckgoalServlet.java

--- src/src/servlet/CheckgoalServlet.java
+++ src/src/servlet/CheckgoalServlet.java
@@ -7,11 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import dao.goalpointDao;
 import dao.loginDao;
-import model.user;
 
 /**
  * Servlet implementation class CheckgoalServlet
@@ -32,8 +30,8 @@ public class CheckgoalServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//セッションスコープからuserIDを取得
-		HttpSession session = request.getSession();
-		user user = (user)session.getAttribute("id");
+		/*HttpSession session = request.getSession();
+		user user = (user)session.getAttribute("id");*/
 
 		//リクエストパラメーターを書く！！
 		request.setCharacterEncoding("UTF-8");
@@ -41,6 +39,7 @@ public class CheckgoalServlet extends HttpServlet {
 		goalpointDao dao = new goalpointDao();
 		loginDao point = new loginDao();
 		int plusPoint = dao.select(id);
+		System.out.println(plusPoint);
 		dao.update(id);
 		boolean update = point.pluspoint_update(id, plusPoint);
 
