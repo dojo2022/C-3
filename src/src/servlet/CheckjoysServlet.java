390e9ea7201c0addded51565b3ace7e2cc495628
dojo2022kawakami <103239295+dojo2022kawakami@users.noreply.github.com>
2022-06-21 14:17:38 +0900

    Update CheckjoysServlet.java

--- src/src/servlet/CheckjoysServlet.java
+++ src/src/servlet/CheckjoysServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.joyspointDao;
+import dao.loginDao;
 
 /**
  * Servlet implementation class CheckjoysServlet
@@ -18,16 +23,32 @@ public class CheckjoysServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//セッションスコープからuserIDを取得
+		HttpSession session = request.getSession();
+		String user = (String)session.getAttribute("id");
+
+				//リクエストパラメーターを書く！！
+			//消費ポイントを返す
+				request.setCharacterEncoding("UTF-8");
+				String  id = request.getParameter("reward_id");
+
+				joyspointDao dao = new joyspointDao();
+				int minuspoint = dao.select(id);
+
+			//ポイントを引き算
+				loginDao point = new loginDao();
+
+				boolean update = point.minuspoint_update(user, minuspoint);
+
+				// アップデートサーブレットにリダイレクトする
+				response.sendRedirect("/app/JousUpdateServlet");
 	}
 
 }
