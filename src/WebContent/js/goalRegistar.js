b8cdba84960f411c297e07eb758c0ccb3cbff782
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-24 13:10:37 +0900

    Update goalRegistar.js

--- src/WebContent/js/goalRegistar.js
+++ src/WebContent/js/goalRegistar.js
@@ -1,3 +1,69 @@
-/**
- * 
- */
+'use strict';
+/*
+var vue = new Vue ({
+  el: '#app',
+  data: {
+    today:''
+   },
+  created: function(){
+    this.today= new Date();
+  }
+ })
+
+ created: function(){
+    let todaySet = new Date();
+    let YYYY = todaySet.getFullYear();
+    let MM = ('00' + (todaySet.getMonth()+1)).slice(-2);
+    let DD = ('00' + todaySet.getDate()).slice(-2);
+    this.today = YYYY + '-' + MM + '-' + DD
+  },*/
+
+function homeRegistar_JSP(){
+  var val_from = document.getElementsByName("starting_date")[0].value;
+  var val_to = document.getElementsByName("ending_date")[0].value;
+
+  if(val_from != "" && val_to != ""){
+
+    // 日付オブジェクトを生成
+    var fromDate = new Date(val_from);
+    var toDate = new Date(val_to);
+    // 開始日と終了日の差を計算
+    var judge = (toDate - fromDate);
+
+    if(judge < 0){
+      alert("終了日には開始日以降の日付を指定してください。");
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return true;
+  }
+
+
+}
+
+function Registar_JSP(){
+  var val_from = document.getElementsByName("starting_date")[0].value;
+  //var val_to = document.getElementsByName("ending_date")[0].value;
+
+  if(val_from != ""){
+
+    // 日付オブジェクトを生成
+    var fromDate = new Date(val_from);
+    var todayDate = new Date();
+    var DAte = todayDate.getDate();
+    //var toDate = new Date(val_to);
+    // 開始日と終了日の差を計算
+    var judge = (DAte - fromDate);
+
+    if(judge < 0){
+      alert("本日以降の日付を指定してください。");
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return true;
+  }
+}
