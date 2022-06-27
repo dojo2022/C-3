a42b5458a1b461efe11b6b62396a2d999b6af27c
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-27 13:06:36 +0900

    Update goalUpdate.js

--- src/WebContent/js/goalUpdate.js
+++ src/WebContent/js/goalUpdate.js
@@ -21,3 +21,27 @@
 			return false;
 		}
 	};
+
+//カレンダー終了日が開始日より早い日付を選択された時のアラート
+function homeRegistar_JSP(){
+  var val_from = document.getElementsByName("starting_date")[0].value;
+  var val_to = document.getElementsByName("ending_date")[0].value;
+
+  if(val_from != "" && val_to != ""){
+
+    // 日付オブジェクトを生成
+    var fromDate = new Date(val_from);
+    var toDate = new Date(val_to);
+    // 開始日と終了日の差を計算
+    var judge = (toDate - fromDate);
+
+    if(judge < 0){
+      alert("終了日には開始日以降の日付を指定してください。");
+      return false;
+    }else{
+      return true;
+    }
+  }else{
+    return true;
+  }
+ }
