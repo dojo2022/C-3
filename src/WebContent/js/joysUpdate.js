b6036b883f8a9ce087047b3d84d8677c9e69dd4d
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-28 14:57:37 +0900

    Update joysUpdate.js

--- src/WebContent/js/joysUpdate.js
+++ src/WebContent/js/joysUpdate.js
@@ -2,24 +2,24 @@
  *
  */
 
- //ログアウトアラート
-	document.getElementById('logout').onclick = function (event) {
-		let re = window.confirm('ログアウトしますか？');
-		if(re === true){
-			window.location.href = 'LogoutServlet';
-			return true;
-		}else {
-			return false;
-		}
-	};
+//ログアウトアラート
+document.getElementById('logout').onclick = function(event) {
+	let re = window.confirm('ログアウトしますか？');
+	if (re === true) {
+		window.location.href = 'LogoutServlet';
+		return true;
+	} else {
+		return false;
+	}
+};
 
-	 //削除アラート
- function chkDelete(ele){
-			let re = window.confirm('この内容で削除しますか？');
-			if(re === true){
-				//window.location.href = 'GoalUpdateServlet';
-				return true;
-			} else {
-				return false;
-			}
-	    }
+//削除アラート
+function chkDelete(ele) {
+	let re = window.confirm('この内容で削除しますか？');
+	if (re === true) {
+		//window.location.href = 'GoalUpdateServlet';
+		return true;
+	} else {
+		return false;
+	}
+}
