32e7813554a10dd3fdf9838bdec97bb4c02d1442
dojo2022-nakashima <103238568+dojo2022-nakashima@users.noreply.github.com>
2022-06-28 14:57:25 +0900

    Update goalRegistar.js

--- src/WebContent/js/goalRegistar.js
+++ src/WebContent/js/goalRegistar.js
@@ -18,65 +18,65 @@ var vue = new Vue ({
     this.today = YYYY + '-' + MM + '-' + DD
   },*/
 
-function homeRegistar_JSP(){
-  var val_from = document.getElementsByName("starting_date")[0].value;
-  var val_to = document.getElementsByName("ending_date")[0].value;
+function homeRegistar_JSP() {
+	var val_from = document.getElementsByName("starting_date")[0].value;
+	var val_to = document.getElementsByName("ending_date")[0].value;
 
-  if(val_from != "" && val_to != ""){
+	if (val_from != "" && val_to != "") {
 
-    // 日付オブジェクトを生成
-    var fromDate = new Date(val_from);
-    var toDate = new Date(val_to);
-    // 開始日と終了日の差を計算
-    var judge = (toDate - fromDate);
+		// 日付オブジェクトを生成
+		var fromDate = new Date(val_from);
+		var toDate = new Date(val_to);
+		// 開始日と終了日の差を計算
+		var judge = (toDate - fromDate);
 
-    if(judge < 0){
-      alert("終了日には開始日以降の日付を指定してください。");
-      return false;
-    }else{
-      return true;
-    }
-  }else{
-    return true;
-  }
+		if (judge < 0) {
+			alert("終了日には開始日以降の日付を指定してください。");
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return true;
+	}
 
 
 }
 
-function Registar_JSP(){
-  var val_from = document.getElementsByName("starting_date")[0].value;
-  //var val_to = document.getElementsByName("ending_date")[0].value;
+function Registar_JSP() {
+	var val_from = document.getElementsByName("starting_date")[0].value;
+	//var val_to = document.getElementsByName("ending_date")[0].value;
 
-  if(val_from != ""){
+	if (val_from != "") {
 
-    // 日付オブジェクトを生成
-    var fromDate = new Date(val_from);
-    var todayDate = new Date();
-    //var toDate = new Date(val_to);
-    var diff = todayDate - fromDate;
-    var diffDays = parseInt(diff / 1000/ 60/ 60/ 24);
-    console.log(diffDays);
-    // 今日と開始日の差を計算
+		// 日付オブジェクトを生成
+		var fromDate = new Date(val_from);
+		var todayDate = new Date();
+		//var toDate = new Date(val_to);
+		var diff = todayDate - fromDate;
+		var diffDays = parseInt(diff / 1000 / 60 / 60 / 24);
+		console.log(diffDays);
+		// 今日と開始日の差を計算
 
-    if(diffDays > 0){
-      alert("本日以降の日付を指定してください。");
-      return false;
-    }else{
-      return true;
-    }
-  }else{
-    return true;
-  }
+		if (diffDays > 0) {
+			alert("本日以降の日付を指定してください。");
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return true;
+	}
 }
 
 //ログアウトアラート
 
-	document.getElementById('logout').onclick = function (event) {
-		let re = window.confirm('ログアウトしますか？');
-		if(re === true){
-			window.location.href = 'LogoutServlet';
-			return true;
-		}else {
-			return false;
-		}
-	};
+document.getElementById('logout').onclick = function(event) {
+	let re = window.confirm('ログアウトしますか？');
+	if (re === true) {
+		window.location.href = 'LogoutServlet';
+		return true;
+	} else {
+		return false;
+	}
+};
